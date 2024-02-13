1600a4bbb4197d9cdedae312896be9fa6af5d220
freamwork97 <jsy11123@naver.com>
2024-02-13 19:08:08 +0900

    2024-02-13

---
+++ 01/inline_ex2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+template <typename T>
+inline T SQUARE(T x) {
+  return x * x;
+}
+
+int main(void) {
+  std::cout << SQUARE(5) << std::endl;
+  std::cout << SQUARE(7.154) << std::endl;
+
+  system("pause");
+  return 0;
+}
