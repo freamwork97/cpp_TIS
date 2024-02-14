8f5a69d11ea0106a2bf938a683b9c262ce0f0c1f
freamwork97 <jsy11123@naver.com>
2024-02-14 17:15:15 +0900

    2024-02-14

---
+++ 01/account_ex.cpp
@@ -0,0 +1,11 @@
+#include <cstring>
+#include <iostream>
+
+const int NAME_LEN = 20;      // customer name
+const int MAX_CUS_NUM = 100;  // Maximum number of customers
+
+void ShowMenu(void);        // menu
+void MakeAccount(void);     // MakeAccount
+void DepositMoney(void);    // DepositMoney
+void WithdrawMoney(void);   // WithdrawMoney
+void ShowAllAccInfo(void);  // All Account show
