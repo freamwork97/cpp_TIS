6c9f66335b6673836301080c6484137e5b9a5dc6
freamwork97 <jsy11123@naver.com>
2024-02-15 09:28:00 +0900

    2024-02-15

--- 01/account_ex.cpp
+++ 01/account_ex.cpp
@@ -1,11 +1,65 @@
 #include <cstring>
 #include <iostream>
 
-const int NAME_LEN = 20;      // customer name
-const int MAX_CUS_NUM = 100;  // Maximum number of customers
+const int NAME_LEN = 20;      // 고객 이름 길이
+const int MAX_CUS_NUM = 100;  // 최대 고객 수
 
-void ShowMenu(void);        // menu
-void MakeAccount(void);     // MakeAccount
-void DepositMoney(void);    // DepositMoney
-void WithdrawMoney(void);   // WithdrawMoney
-void ShowAllAccInfo(void);  // All Account show
+void ShowMenu(void);  // 메뉴 출력
+// void MakeAccount(void);     // 계좌 생성
+// void DepositMoney(void);    // 입금
+// void WithdrawMoney(void);   // 출금
+// void ShowAllAccInfo(void);  // 모든 계좌 정보 출력
+
+enum { MAKE = 1, DEPOSIT, WITHDRAW, INQUIRE, EXIT };
+
+typedef struct {           // 구조체 이름 생략 가능
+  int accID;               // 계좌 번호
+  int balance;             // 잔액
+  char cusName[NAME_LEN];  // 고객 이름
+} ACCOUNT;
+
+ACCOUNT account[MAX_CUS_NUM];  // 계좌 처리
+int accNum = 0;                // 계좌 인덱스
+
+int main(void) {
+  int choice;
+
+  while (1) {
+    ShowMenu();
+    std::cout << "choice: ";
+    std::cin >> choice;
+    std::cout << std::endl;
+
+    switch (choice) {
+      case MAKE:
+        // MakeAccount();
+        break;
+      case DEPOSIT:
+        // DepositMoney();
+        break;
+      case WITHDRAW:
+        // WithdrawMoney();
+        break;
+      case INQUIRE:
+        // ShowAllAccInfo();
+      case EXIT:
+        return 0;
+      default:
+        std::cout << "Invalid input." << std::endl;
+        break;
+    }
+  }
+  system("pause");
+  return 0;
+}
+
+void ShowMenu(void) {
+  std::cout << std::endl;
+  std::cout << "--------Menu------------" << std::endl;
+  std::cout << "1. MakeAccount" << std::endl;
+  std::cout << "2. DepositMoney" << std::endl;
+  std::cout << "3. WithdrawMoney" << std::endl;
+  std::cout << "4. ShowAllAccInfo" << std::endl;
+  std::cout << "5. EXIT" << std::endl;
+  std::cout << std::endl;
+}
